37a8586cf1b1bf9bae14c030a44bbf9a18d3d9b0
abdullahhussain706 <abdullah706706@gmail.com>
2025-01-28 22:47:10 +0500

    Sign up & sign in Complete

--- SignInScript.js
+++ SignInScript.js
@@ -1,13 +1,30 @@
-const wrapper=document.querySelector('.wrapper');
-const signUpLink=document.querySelector('.signUp-link');
-const signINLink=document.querySelector('.signIn-link');
+const wrapper = document.querySelector('.wrapper');
+const signUpLink = document.querySelector('.signUp-link');
+const signInLink = document.querySelector('.signIn-link');
 
-signUpLink.addEventListener('click',()=>{
+// Get the show password checkboxes
+const showPasswordSignUp = document.querySelector('#showPasswordSignUp');
+const showPasswordSignIn = document.querySelector('#showPasswordSignIn');
+
+// Get the password input fields
+const passwordSignUp = document.querySelector('.password-signup');
+const passwordSignIn = document.querySelector('.password-signin');
+
+// Toggle password visibility
+showPasswordSignUp.addEventListener('change', () => {
+    passwordSignUp.type = showPasswordSignUp.checked ? 'text' : 'password';
+});
+
+showPasswordSignIn.addEventListener('change', () => {
+    passwordSignIn.type = showPasswordSignIn.checked ? 'text' : 'password';
+});
+
+signUpLink.addEventListener('click', () => {
     wrapper.classList.add('animate-signIn');
     wrapper.classList.remove('animate-signUp');
 });
 
-signInLink.addEventListener('click',()=>{
+signInLink.addEventListener('click', () => {
     wrapper.classList.add('animate-signUp');
     wrapper.classList.remove('animate-signIn');
 });
